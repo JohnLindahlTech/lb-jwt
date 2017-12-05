7f31bef9a12b386fa6bddca75be4b79144eb6618
John <john@bofink.com>
2017-12-05 18:54:17 +0100

    fix: Actually working.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
+import assert from 'assert';
 import Debug from 'debug';
+import { ExtractJwt } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
 
 const debug = Debug('lb-jwt');
 
-debug('hello!');
+export const tokenExtractor = ExtractJwt.fromExtractors([
+  ExtractJwt.fromAuthHeaderAsBearerToken(),
+  ExtractJwt.fromUrlQueryParameter('access_token'),
+]);
+
+export function verifyJWT(secret, token){
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (error, payload) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(payload);
+    });
+  });
+}
+
+export function attachUser(req, user){
+  req.user = user;
+}
+
+export async function requestHandler(req, res, options){
+  const token = tokenExtractor(req);
+  if(!token){
+    return;
+  }
+  const payload = await verifyJWT(options.secret, token);
+  attachUser(req, payload);
+}
+
+function middleware(options = {}){
+  assert(typeof options.jwtSecret === 'string', 'Must supply options.jwtSecret as a string');
+
+  return (req, res, next) => {
+    requestHandler(req, res, options)
+      .then(() => next())
+      .catch(error => next(error))
+
+  }
+}
+
+export default middleware;
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,70 @@
 import test from 'ava';
+import {
+  verifyJWT,
+  attachUser,
+  requestHandler,
+  tokenExtractor,
+} from './index';
+import jwt from 'jsonwebtoken';
 
-test('foo', (t) => {
-  t.pass();
+const user = {
+  name: 'John Doe',
+  sub: '1234567890',
+  iat: 1512495411,
+};
+const secret = 'secret';
+
+test('verifyJWT should return the payload', async (t) => {
+  const token = jwt.sign(user, secret);
+  const result = await verifyJWT(secret, token);
+  t.deepEqual(result, user);
+});
+
+test('attachUser should be able to attach a user', (t) => {
+  const req = {};
+  attachUser(req, user)
+  t.deepEqual(req, { user })
+});
+
+test('tokenExtractor should be able to get the jwt token from bearer', t => {
+  const req = {
+    headers: {
+      authorization: 'Bearer token',
+    },
+    url: '',
+  };
+  t.is(tokenExtractor(req), 'token');
+});
+
+test('tokenExtractor should be able to get the jwt token from url query param', t => {
+  const req = {
+    headers: {},
+    url: '/not/used?access_token=token'
+  };
+  t.is(tokenExtractor(req), 'token');
+});
+
+
+test('requestHandler should be able to create a user property', async (t) => {
+  const token = jwt.sign(user, secret);
+  const req = {
+    headers: {
+      authorization: `Bearer ${token}`,
+    }
+  };
+  const res = {};
+  await requestHandler(req, res, { secret });
+  t.deepEqual(req.user, user);
+});
+
+test('requestHandler should do nothing if jwt is missing.', async (t) => {
+  const token = jwt.sign(user, secret);
+  const req = {
+    headers: {
+    },
+    url: '',
+  };
+  const res = {};
+  await requestHandler(req, res, { secret });
+  t.is(typeof req.user, 'undefined');
 });
